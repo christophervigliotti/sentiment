748f0c0c88fd120733288dcdd05ad3c99f998b54
Christopher Vigliotti <christopher@christophervigliotti.com>
2019-04-30 15:13:38 -0400

    wip

--- lib/sentiment_parser.rb
+++ lib/sentiment_parser.rb
@@ -1,20 +1,37 @@
 
 class SentimentParser
+
   def self.positive_words
     File.readlines('data/positive_words.txt')
     #File.readlines('data/positive_words_vigliotti.txt')
   end
+
   def self.negative_words
     File.readlines('data/negative_words.txt')
     #File.readlines('data/negative_words_vigliotti.txt')
   end
+
   def self.analyze(words)
+    positive_words_array = positive_words
+    negative_words_array = negative_words
+    results = {:positive => 0, :negative => 0}
+    words.downcase.split(' ').each do |word|
+      results[:positive] +=1 if positive_words_array.include?(word)
+      results[:negative] +=1 if negative_words_array.include?(word)
+    end
+    return results
+  end
+
+  # this works
+  def self.analyze_version_02(words)
     positive_words_array = positive_words
     negative_words_array = negative_words
     positive_counter = 0
     negative_counter = 0
     results = {:positive => 0, :negative => 0}
     words.downcase.split(' ').each do |word|
+      positive_counter +=1 if positive_words_array.include?(word)
+      negative_counter +=1 if negative_words_array.include?(word)
       positive_counter +=1 if positive_words_array.include?(word)
       negative_counter +=1 if negative_words_array.include?(word)
     end
