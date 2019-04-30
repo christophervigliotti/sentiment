93622cce46afd3a6997997af4fb849af9a9cc3c5
Christopher Vigliotti <christopher@christophervigliotti.com>
2019-04-30 15:09:33 -0400

    wip

--- lib/sentiment_parser.rb
+++ lib/sentiment_parser.rb
@@ -11,17 +11,20 @@ class SentimentParser
   def self.analyze(words)
     positive_words_array = positive_words
     negative_words_array = negative_words
+    positive_counter = 0
+    negative_counter = 0
     results = {:positive => 0, :negative => 0}
-    words.downcase.split(/[\s,',-]/).each do |word|
+    words.downcase.split(' ').each do |word|
       positive_words_array.each do |positive_word|
-        puts "does #{word} equal #{positive_word}"
-
-        # this is displaying false even when the words match
-        puts positive_word.strip == word.strip
-
+        positive_counter += 1 if positive_word.strip == word.strip
+      end
+      negative_words_array.each do |negative_word|
+        negative_counter += 1 if negative_word.strip == word.strip
       end
     end
 
+    results[:positive] = positive_counter
+    results[:negative] = negative_counter
     return results
   end
 end
